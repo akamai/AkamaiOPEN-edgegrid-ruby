5b81b8f229dfef73eabc6a6e55c70cc51cb03554
mniemczy <mniemczy@akamai.com>
2024-07-23 12:37:27 +0200

    DXISSUE-58: Update copyright info

--- lib/akamai/edgegrid.rb
+++ lib/akamai/edgegrid.rb
@@ -7,7 +7,7 @@
 #
 # == License
 #
-#   Copyright 2014-2015 Akamai Technologies, Inc. All rights reserved.
+#   Copyright 2024 Akamai Technologies, Inc. All rights reserved.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
--- test/test_edgerc.rb
+++ test/test_edgerc.rb
@@ -4,7 +4,7 @@
 #
 # For more information visit https://developer.akamai.com
 #
-#   Copyright 2014 Akamai Technologies, Inc.  All rights reserved.
+#   Copyright 2024 Akamai Technologies, Inc.  All rights reserved.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
